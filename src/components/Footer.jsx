522946430a46c9a920609f86032ebf8f7d510b65
camille <camille.seaberry@gmail.com>
2018-01-17 19:00:43 +0000

    Change download link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const text = 'Source: DataHaven analysis (2017) of US Census Bureau American Com
 const Footer = () => (
 	<div className="Footer">
 		<p>{text}</p>
-		<p><a href="https://github.com/camille-s/waterbury2016/blob/master/public/data/2016_wby_display.csv" target="_blank">Download all Waterbury 2016 profile data</a></p>
+		<p><a href="https://github.com/ct-data-haven/waterbury2016/blob/master/public/data/2016_wby_display.csv" target="_blank">Download all Waterbury 2016 profile data</a></p>
 	</div>
 );
 
